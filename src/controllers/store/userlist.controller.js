0201162f937b653514f88111951cfccf57ab68b8
Prabha Panneer <prabha1094@gmail.com>
2021-11-22 18:09:17 +0530

    upload image

--- src/controllers/store/userlist.controller.js
+++ src/controllers/store/userlist.controller.js
@@ -1,10 +1,12 @@
 "use strict";
 const mongoose = require('mongoose');
+const fs = require("fs");
+const path = require('path');
 const bcrypt = require("bcrypt-nodejs");
 const saltRounds = bcrypt.genSaltSync(10);
 const userlist = require("../../models/userlist.model");
 const materials = require("../../models/materials.model");
-
+const imgUploadService = require("../../../services/img_upload.service");
 
 exports.list = (req, res) => {
     userlist.find({ store_id: mongoose.Types.ObjectId(req.id), status : "active" }, function(err, response) {
@@ -15,24 +17,41 @@ exports.list = (req, res) => {
 
 exports.add = (req, res) => {
     console.log(req.id);
-    console.log(req.body)
+    // console.log(req.body);
+    // let rootPath = 'uploads/'+req.id+'/category';
     req.body.store_id  = req.id;
     req.body.username = req.body.email;
     req.body.password = bcrypt.hashSync("welcome2erp", saltRounds);
-    console.log(req.body);
-    userlist.findOne({ store_id: mongoose.Types.ObjectId(req.id), email : req.body.email }, function(err, response) {
-        if(!err && response)
+    // console.log(req.body);
+
+        userlist.findOne({ store_id: mongoose.Types.ObjectId(req.id), email : req.body.email }, function(err, response) 
         {
-            res.json({ status: false, error: err, message: "user already exsist" });
-        }
-        else {
-            userlist.create(req.body, function(err, response) {
-                if(!err && response) { res.json({ status: true, data: response }); }
-                else { res.json({ status: false, error: err, message: "Unable to add" }); }
-            });
-        }
-    });
+            if(!err && response)
+            {
+                res.json({ status: false, error: err, message: "user already exsist" });
+            }
+            else 
+            {
+                let rootPath = 'uploads/'+req.id+'/user/';
+                singleFileUpload(req.body.profileimage, rootPath).then((profileimage) => 
+                {
+                    req.body.profileimage = profileimage;
+                    userlist.create(req.body, function(err, response) 
+                    {
+                        if(!err && response) 
+                        {
+                        res.json({ status: true, data: response }); 
+                        }
+                        else { res.json({ status: false, error: err, message: "Unable to add" }); }
+                    });
+                });
+            }
+        });                       
+
+
+    
 }
+
 
 exports.details = (req, res) => {
 
@@ -113,5 +132,35 @@ exports.hard_remove = (req, res) => {
         else {
             res.json({ status: false, error: err, message: "Invalid blog" });
         }
+    });
+}
+
+function singleFileUpload(image, rootPath) 
+{
+    return new Promise((resolve, reject) => {
+        let fileType = ""; let base64Data = "";
+        if(image.indexOf('png;base64') > -1) {
+            fileType = ".png";
+            base64Data = image.replace(/^data:image\/png;base64,/, "");
+        } else {
+            fileType = ".jpeg";
+            base64Data = image.replace(/^data:image\/jpeg;base64,/, "");
+        }
+        let randomName = 'profile-'+new Date().valueOf()+'-'+Math.floor(Math.random() * Math.floor(999999));
+        let fileName = rootPath+randomName+fileType;
+        if(!fs.existsSync(rootPath)) {
+            fs.mkdir(rootPath, { recursive: true }, (err) => {
+                if(!err) {
+        fs.writeFile(fileName, base64Data, 'base64', function(err) {
+            if(!err) { 
+                resolve(fileName); 
+            }
+            else { reject("logo upload error"); }
+        });
+    }
+    else { resolve(null); }
+});
+}
+        
     });
 }
